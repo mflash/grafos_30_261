0ca271c017fea9d69b700494ff4262c737ecbd41
Marcelo Cohen <marcelo.cohen@pucrs.br>
2026-06-08 20:42:29 -0300

    Comparação de caminhos mínimos (Dijkstra x Floyd-Warshall)

---
+++ AppComparaCaminhosMinimos.java
@@ -0,0 +1,23 @@
+public class AppComparaCaminhosMinimos {
+   
+    public static void main(String[] args) {
+        EdgeWeightedDigraph g = new EdgeWeightedDigraph("exemplos/mediumEWG.txt");
+        Digraph g2 = DigraphGenerator.complete(40);
+        System.out.println(g2.toDot());
+
+        long start = System.nanoTime();
+        for(String v: g.getVerts()) {
+            DijkstraSP dij = new DijkstraSP(g, v);
+        }
+        long end = System.nanoTime();
+        double delta = (end-start)/1e9;
+        System.out.printf("Tempo Dijkstra: %f\n",delta);
+
+        start = System.nanoTime();
+        FloydWarshallSP fw = new FloydWarshallSP(g);
+        end = System.nanoTime();
+        delta = (end-start)/1e9;
+        System.out.printf("Tempo F-W:      %f\n",delta);
+
+    }
+}
